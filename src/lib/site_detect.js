9b190337944cdb4c1f9634b1fef92038d54bf17b
Jayin Ton <tonjayin@gmail.com>
2016-10-14 09:36:11 +0800

    Fix site detect not works

--- src/lib/site_detect.js
+++ src/lib/site_detect.js
@@ -3,16 +3,16 @@
  * 检测网站类型
  */
 module.exports = function(url){
-    if(url.indexOf('amazon.com')){
+    if(url.indexOf('amazon.com') > 0){
         return 'amazon'
     }
-    if(url.indexOf('bestbuy.com')){
+    if(url.indexOf('bestbuy.com') > 0){
         return 'bestbuy'
     }
-    if(url.indexOf('costco.com')){
+    if(url.indexOf('costco.com') > 0){
         return 'costco'
     }
-    if(url.indexOf('6pm.com')){
+    if(url.indexOf('6pm.com') > 0){
         return 'sixpm'
     }
 
