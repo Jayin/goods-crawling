26295ebdbc7ecfeaeb825c743a8f00fcf23cd4e8
Jayin Ton <tonjayin@gmail.com>
2017-01-06 22:09:01 +0800

    test: 重构测试

--- test/costco.js
+++ test/costco.js
@@ -1,36 +1,19 @@
-'use strict'
 
 const fs = require('fs')
 const path = require('path')
-
-const costco = require('../src/parser/costco.js')
-
-const {createNightmare} = require('../src/main_nightmare')
+const crawler = require('../src/crawler/costco')
+const parser = require('../src/parser/costco')
 
 let url = 'http://www.costco.com/Diamondback-Edgewood-Hybrid-Bike-2016.product.100242696.html'
-let nightmare = createNightmare({show: false})
-nightmare
-  .useragent('Chrome')
 
-  .goto(url)
-  // .type('form[action*="/search"] [name=p]', 'github nightmare')
-  // .click('form[action*="/search"] [type=submit]')
-  // .wait('#main')
-  // .inject('js', path.join(__dirname, "preload/jquery.js"))
-  // .wait('#brand')
-  .end()
-  .evaluate(function () {
-    return document.documentElement.outerHTML
-  })
-  .then(function (result) {
-    // console.log(result)
-    // fs.writeFileSync(path.join(__dirname, 'tb.html'), result, {encoding: 'utf8'})
-    return costco(result)
-  })
-  .then(function(result){
+crawler.fetch(url, {show: true}, function(err, html){
+    if(err){
+      console.log(err);
+      return;
+    }
+    // console.log(html)
+    let result = parser(html)
+
     console.log(result)
     fs.writeFileSync(path.join(__dirname, '../result/costco.json'), JSON.stringify(result,null, 4), {encoding: 'utf8'})
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error)
-  })
+})
--- test/sixpm.js
+++ test/sixpm.js
@@ -1,36 +1,19 @@
-'use strict'
 
 const fs = require('fs')
 const path = require('path')
-
-const sixpm = require('../src/parser/sixpm.js')
-
-const {createNightmare} = require('../src/main_nightmare')
+const crawler = require('../src/crawler/sixpm')
+const parser = require('../src/parser/sixpm')
 
 let url = 'http://www.6pm.com/u-s-polo-assn-slim-straight-corduroy-five-pocket-jeans-in-mood-indigo-mood-indigo'
-let nightmare = createNightmare({show: false})
-nightmare
-  .useragent('Chrome')
 
-  .goto(url)
-  // .type('form[action*="/search"] [name=p]', 'github nightmare')
-  // .click('form[action*="/search"] [type=submit]')
-  // .wait('#main')
-  // .inject('js', path.join(__dirname, "preload/jquery.js"))
-  // .wait('#brand')
-  .end()
-  .evaluate(function () {
-    return document.documentElement.outerHTML
-  })
-  .then(function (result) {
-    // console.log(result)
-    // fs.writeFileSync(path.join(__dirname, 'tb.html'), result, {encoding: 'utf8'})
-    return sixpm(result)
-  })
-  .then(function(result){
+crawler.fetch(url, {show: true}, function(err, html){
+    if(err){
+      console.log(err);
+      return;
+    }
+    // console.log(html)
+    let result = parser(html)
+
     console.log(result)
     fs.writeFileSync(path.join(__dirname, '../result/sixpm.json'), JSON.stringify(result,null, 4), {encoding: 'utf8'})
-  })
-  .catch(function (error) {
-    console.error('Search failed:', error)
-  })
+})
