5b0bdc90c48eef958d66b5294d19c03052ff4281
Jayin Ton <tonjayin@gmail.com>
2017-01-24 20:24:45 +0800

    crawler/amazon: 改进user agent 筛选条件

--- src/crawler/amazon.js
+++ src/crawler/amazon.js
@@ -6,10 +6,23 @@ function fetch (url, options, cb) {
   let opt = {
       url: url,
       headers: {
-        'User-Agent': random_useragent.getRandom(function(ua){
-             //取linux + windows 的PC浏览器
-             return (ua.browserName == 'Chrome' || ua.browserName == 'Firefox') && ua.deviceType != 'mobile';
-         }),
+        'User-Agent': random_useragent.getRandom(function (ua) {
+            // 取Chrome Firefox的PC浏览器
+            let status = (ua.browserName == 'Chrome' || ua.browserName == 'Firefox')
+            //  排除手机设备 不包含mobile，不含ios android 系统
+            status = status && ua.deviceType != 'mobile'
+            status = status && ua.folder.toLowerCase().indexOf('mobile') == -1
+            status = status && ua.userAgent.toLowerCase().indexOf('mobile') == -1
+            status = status && ua.description.toLowerCase().indexOf('mobile') == -1
+            // 排除Android ios 系统
+            status = status && ua.osName.toLowerCase().indexOf('android') == -1
+            status = status && ua.osName.toLowerCase().indexOf('ios') == -1
+
+            // 排除Spiders
+            status = status && ua.folder.toLowerCase().indexOf('spiders') == -1
+
+            return status
+        }),
         'Host': 'www.amazon.com',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding': 'identity',
